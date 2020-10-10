4f606a2f112dab7670df8da8200ffa728180d0a8
sachin <chinsa.kulkarni29@gmail.com>
2020-10-10 16:10:59 +0530

    this is second commit post change in clone project

---
+++ src/gitclonechange.java
@@ -0,0 +1,15 @@
+
+public class gitclonechange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		demotest();
+	}
+
+	public static void demotest()
+	{
+		System.out.println("new class added in clone project");
+		System.out.println("will add new comment line in the clone project");
+	}
+}
+
